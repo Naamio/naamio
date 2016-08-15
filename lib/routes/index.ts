1fc4eee89559f3121e1027e036e8efa8f312b47d
Gabrielle Iskandar <gabiskandar@gmail.com>
2016-08-15 19:08:44 +0300

    Added routing for about and contact.

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -10,7 +10,7 @@ const router = express.Router();
  * Retrieves the landing page for the app.
  * @version 0.2.0
  * @param {express.Request} req Request object.
- * @param {express.Request} res Response object.
+ * @param {express.Response} res Response object.
  */
 const getLanding = (req: express.Request, res: express.Response) => {
   if (typeof (<any>req).user == 'undefined') {
@@ -25,6 +25,40 @@ const getLanding = (req: express.Request, res: express.Response) => {
   });
 };
 
+/**
+ * Retrieves the about page for the app.
+ * @version 0.2.0
+ * @param {express.Request} req Request object.
+ * @param {express.Response} res Response object.
+ */
+
+const getAbout = (req: express.Request, res: express.Response) => {
+
+  res.render('about', {
+    title: 'About Us',
+    header: true,
+    footer: true
+  });
+};
+
+/**
+ * Retrieves the contact page for the app.
+ * @version 0.2.0
+ * @param {express.Request} req Request object.
+ * @param {express.Response} res Response object.
+ */
+
+const getContact = (req: express.Request, res: express.Response) => {
+
+  res.render('contact', {
+    title: 'Contact Us',
+    header: true,
+    footer: true
+  });
+};
+
 router.get('/', getLanding);
+router.get('/about', getAbout);
+router.get('/contact', getContact);
 
 export = router;
