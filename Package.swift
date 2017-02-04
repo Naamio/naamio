a40de413ba0237296e519d0f2cf7b83ffe8483c0
Phil J. Laszkowicz <phil@fillip.pro>
2017-02-04 20:50:42 +0200

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             name: "NaamioCore",
             dependencies: [])],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 1)],
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 6)],
     exclude: ["Makefile"])
