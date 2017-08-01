483e7eb490b0784a70b30930ea41199e64dc6aff
Phil J. Laszkowicz <phil@fillip.pro>
2017-07-31 20:16:44 -0700

    Fixed unsatisfiable dependency issue.

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
             dependencies: [
             ])],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/OmnijarStudio/malline", majorVersion: 0, minor: 2)
+          .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 7),
+          .Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 7),
+          .Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 9),
+          .Package(url: "https://github.com/OmnijarStudio/malline", majorVersion: 0, minor: 2)
     ],
     exclude: ["Makefile"])
