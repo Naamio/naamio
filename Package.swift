126c69dc2baf384562ac521f6f7745f3dd7a6fb1
Phil J. Laszkowicz <phil@fillip.pro>
2017-11-02 15:36:08 +0200

    WIP: Updating for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Naamio",
+    dependencies: [
+          Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 7),
+          Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 7),
+          Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 9),
+          Package(url: "https://github.com/OmnijarStudio/malline", majorVersion: 0, minor: 2)
+    ],
     targets: [
         Target(
             name: "Naamio",
@@ -16,11 +23,4 @@ let package = Package(
         Target(
             name: "NaamioTemplateEngine",
             dependencies: [
-            ])],
-    dependencies: [
-          .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 7),
-          .Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 7),
-          .Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 9),
-          .Package(url: "https://github.com/OmnijarStudio/malline", majorVersion: 0, minor: 2)
-    ],
-    exclude: ["Makefile"])
+            ])])
