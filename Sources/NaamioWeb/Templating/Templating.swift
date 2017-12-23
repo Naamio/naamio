41a45b6414311fa87328c3bc4456bb18f165f9ca
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-23 12:57:26 +0200

    Moved NaamioWeb use of Templating to default instance.

--- Sources/NaamioWeb/Templating/Templating.swift
+++ Sources/NaamioWeb/Templating/Templating.swift
@@ -4,23 +4,20 @@ import NaamioCore
 /// Templating provides the tools necessary to take content
 /// and provide it in a structured method to the end-user.
 class Templating {
+
+    static let `default` = Templating()
     
-    private static let templateSuffix = "html"
+    private let templateSuffix = "html"
     
-    static var templates: [String] = {
-        let resources = Resources()
-        
-        return resources.getResources(from: Templating.path, withSuffix: templateSuffix)
-    }()
+    var templates: [String] = [String]()
 
     /// The path of the templates.
-    static var path: String {
-        return Config.settings["naamio.templates"] as? String ?? "_templates/leaf"
-    }
+    var path: String = ""
 
-    /// Loads the templates into memory for safe and efficient
-    /// processing at runtime.
-    static func load() {
+    init() {
+        let resources = Resources()
         
+        templates = resources.getResources(from: path, withSuffix: templateSuffix)
+        path = Config.settings["naamio.templates"] as? String ?? "_templates/leaf"
     }
 }
