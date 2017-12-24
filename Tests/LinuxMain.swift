cbc4e7c2e85c23324602d26f35c5451ccee207e0
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-24 17:14:38 +0200

    Corrected Linux test suite.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,6 @@ import XCTest
 @testable import NaamioWebTests
 
 XCTMain([
-    testCase(TestTemplating.allTests)
-    testCase(TestServer.allTests)
+    testCase(TestServer.allTests),
+    testCase(TestTemplating.allTests),
 ])
